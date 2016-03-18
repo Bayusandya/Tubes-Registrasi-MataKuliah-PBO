ebfbbe350433622bdb1b20c2f2c3a1e8b425fc70
Computer <Computer@USER>
2016-03-18 10:02:34 +0700

    tubesss

--- tubesss/src/tubesss/Orang.java
+++ tubesss/src/tubesss/Orang.java
@@ -9,8 +9,28 @@ package tubesss;
  *
  * @author ANZYM
  */
-public class Orang {
-    Public Orang () {
-        
+public class Mahasiswa extends Orang {
+    private String nama;
+    pivate String nim;
+    
+    public orang (string nama, string nim) {
+        this.nama = nama;
+        this.nim = nim; 
+    }
+    
+    public void setNamaMahasiswa (string nama) {
+        this.nama = nama;
+    }
+    
+    public void setNimMahasiwa (string nama) {
+        this.nim = nim;
+    }
+    
+    public string getNimMahasiswa() {
+        return nim;
+    }
+    
+    public string getNamaMahasiswa() {
+        return nama;
     }
 }
